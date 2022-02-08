983c754bdc965a48e7f199753cf9a7dd71f9562a
Sahil Narain <sahil.narain@gmail.com>
2022-02-08 13:31:42 +0530

    Assume DB name to be boilerplate

--- tests/setup/data/teardown.sql
+++ tests/setup/data/teardown.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS maruti;
-CREATE DATABASE maruti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+DROP DATABASE IF EXISTS boilerplate;
+CREATE DATABASE boilerplate CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
