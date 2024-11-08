fec9d3f4df4dccb7cde3dc9f255ea19a7437bc39
Sahil Narain <sahil.narain@gmail.com>
2024-11-08 09:42:31 +0000

    Stricter linting, silent run prettier

--- scripts/beautify.sh
+++ scripts/beautify.sh
@@ -1,2 +1,3 @@
-./node_modules/prettier/bin-prettier.js --config ./.prettierrc --write app/**/*.js tests/**/**/*.js
+echo "Running prettier..."
+./node_modules/prettier/bin-prettier.js --config ./.prettierrc --write app/**/*.js tests/**/**/*.js --loglevel silent
 git add $(git diff --name-only --diff-filter d | grep '\.js$' | xargs)
