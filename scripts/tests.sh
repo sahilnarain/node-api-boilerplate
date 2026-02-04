9dbcd617fc364eabe225ca48196f1e4d8ac6c8c5
Sahil Narain <sahil.narain@gmail.com>
2026-02-04 22:31:59 +0000

    Transient database for testing

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
 
-NODE_ENV=test
-node --test --test-concurrency=1 --experimental-test-coverage --test-force-exit --test-timeout 30000 tests/api/*.test.js
+export NODE_ENV=test
+
+# TODO: Read the config for db connection string
+
+CONNECTION_STRING=`awk '/const mysqlConnectionString\s+=\s+{/,/};/' app/configs/environments/test.js`
+DB_HOST=`echo $CONNECTION_STRING | awk -F"host: '" '{print $2}' | awk -F"'" '{print $1}'`
+DB_USER=`echo $CONNECTION_STRING | awk -F"user: '" '{print $2}' | awk -F"'" '{print $1}'`
+DB_PASS=`echo $CONNECTION_STRING | awk -F"password: '" '{print $2}' | awk -F"'" '{print $1}'`
+DB_DATABASE=`echo $CONNECTION_STRING | awk -F"database: '" '{print $2}' | awk -F"'" '{print $1}'`
+DB_PORT=`echo "$CONNECTION_STRING" | awk -F"port: |," '{print $2}'`
+TEST_DATABASE_NAME="$DB_DATABASE"_`cat /dev/urandom | tr -dc 'a-f0-9' | head -c 32`
+
+SETUP_DB_COMMAND="CREATE DATABASE IF NOT EXISTS $TEST_DATABASE_NAME; GRANT ALL PRIVILEGES ON $TEST_DATABASE_NAME TO '$DB_USER'@'%'; FLUSH PRIVILEGES;"
+mysql -h $DB_HOST -u $DB_USER --port $DB_PORT --password=$DB_PASS --database $DB_DATABASE -e "$SETUP_DB_COMMAND" &> /dev/null
+sed -i "s/$DB_DATABASE/$TEST_DATABASE_NAME/" app/configs/environments/test.js
+
+node server.js &> /dev/null &
+NODE_SERVER_PID=`echo $!`
+
+while :
+do
+  curl -X GET http://localhost:3000/ping &> /dev/null
+  if [[ $? -eq 0 ]]
+  then
+    break
+  else
+    sleep 0.25
+  fi
+done
+
+TEST_DATABASE_NAME=$TEST_DATABASE_NAME node --test --test-concurrency=1 --experimental-test-coverage --test-force-exit --test-timeout 30000 tests/api/*.test.js
+
+sed -i "s/$TEST_DATABASE_NAME/$DB_DATABASE/" app/configs/environments/test.js
+TEARDOWN_DB_COMMAND="DROP DATABASE IF EXISTS $TEST_DATABASE_NAME;"
+mysql -h $DB_HOST -u $DB_USER --port $DB_PORT --password=$DB_PASS --database $DB_DATABASE -e "$TEARDOWN_DB_COMMAND" &> /dev/null
+
+if [[ $NODE_SERVER_PID ]]
+then
+  kill -9 $NODE_SERVER_PID
+fi
--- tests/setup/setup.js
+++ tests/setup/setup.js
@@ -9,11 +9,16 @@ const dbSetup = './docs/db/sql-init.sql';
 const dbTeardown = './tests/setup/data/teardown.sql';
 
 const setup = async () => {
+  if (!process.env.TEST_DATABASE_NAME) {
+    console.log('TEST_DATABASE_NAME not set, quitting.');
+    process.exit();
+  }
+
   let dbSetupSql = fs.readFileSync(dbSetup).toString();
 
-  dbSetupSql = dbSetupSql.replace('IF EXISTS boilerplate', 'IF EXISTS boilerplate_test');
-  dbSetupSql = dbSetupSql.replace('CREATE DATABASE IF NOT EXISTS boilerplate', 'CREATE DATABASE IF NOT EXISTS boilerplate_test');
-  dbSetupSql = dbSetupSql.replace('USE boilerplate', 'USE boilerplate_test');
+  dbSetupSql = dbSetupSql.replace('IF EXISTS boilerplate', `IF EXISTS ${process.env.TEST_DATABASE_NAME}`);
+  dbSetupSql = dbSetupSql.replace('CREATE DATABASE IF NOT EXISTS boilerplate', `CREATE DATABASE IF NOT EXISTS ${process.env.TEST_DATABASE_NAME}`);
+  dbSetupSql = dbSetupSql.replace('USE boilerplate', `USE ${process.env.TEST_DATABASE_NAME}`);
 
   await config.knex.raw(dbSetupSql);
 
@@ -21,9 +26,14 @@ const setup = async () => {
 };
 
 const teardown = async () => {
+  if (!process.env.TEST_DATABASE_NAME) {
+    console.log('TEST_DATABASE_NAME not set, quitting.');
+    process.exit();
+  }
+
   let teardownSql = fs.readFileSync(dbTeardown).toString();
 
-  teardownSql = teardownSql.replace(/boilerplate/g, 'boilerplate_test');
+  teardownSql = teardownSql.replace(/boilerplate/g, `${process.env.TEST_DATABASE_NAME}`);
 
   await config.knex.raw(teardownSql);
 
